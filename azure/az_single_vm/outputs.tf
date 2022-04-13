cb7b47164fac3aad4ab5ad635ee2335fb62c0edc
ymelo <yuri@tenthline.com>
2022-04-13 12:18:07 -0400

    add vmname

--- azure/az_single_vm/outputs.tf
+++ azure/az_single_vm/outputs.tf
@@ -4,10 +4,21 @@
 output "group_name" {
   value = local.group_name
 }
+output "vm_name" {
+  value = local.vm_full_name
+}
 
-output "vm_ip"{
+output "internal_ip"{
   value = azurerm_network_interface.vm_nic.ip_configuration[0].private_ip_address
 }
+
+output "external_ip"{
+  value = azurerm_public_ip.pip.ip_address
+}
+output "username"{
+  value = var.username
+}
+
 output "ssh_connection"{
   value = "ssh ${var.username}@${azurerm_public_ip.pip.ip_address}"
 }
